f35e10e200f44e562b39c1ac3bdf61756be084e1
Christopher McGlone <59942651+Mcglonec2@users.noreply.github.com>
2020-01-23 21:30:12 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+	
+		public static void main(String[] args) {
+			
+			System.out.println("Christopher McGlone");
+			System.out.println("Hello.java");
+			System.out.println("I'm saying Hello with code");
+			System.out.println("Jan 15, 2020");
+			System.out.println("CMSC-255-901");
+			System.out.println();
+		}
+}
