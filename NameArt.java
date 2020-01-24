cee1a31f100ec8eaa3d87b8c43e0ba212eee3a9a
Christopher McGlone <59942651+Mcglonec2@users.noreply.github.com>
2020-01-23 21:30:52 -0500

    Add files via upload

---
+++ NameArt.java
@@ -0,0 +1,32 @@
+/************************************************************************
+ * NameArt.java
+ ************************************************************************
+ * This project displays my name 
+ * ----------------------------------------------------------------------
+ * Christopher McGlone
+ * January 23, 2020
+ * CMSC-255-901
+ ************************************************************************/
+ 
+ public class NameArt {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Christopher McGlone");
+		System.out.println("Project 1 - Name in Code");
+		System.out.println("This project outputs my name in a unique way on the display screen");
+		System.out.println("January 20, 2020");
+		System.out.println("CMSC-255-901");
+		System.out.println();
+		System.out.println("      RRRRRRRRRRRRRRR         EEEEEEE     EEEEEEE     HHHHHHHHHHHHHH      PPPPPP       OOOOOOOOOOO       TTTTTTTTTTTTTT     SSSSSSSSSSSSSS       IIIIIIIIIIIIII     RRRRRRR     RRRRRRR     HHHHHHHHHHHHHHHHHHH    CCCCCCCCCCCCCC");
+		System.out.println("    RRRRRRRRRRRRRRRRRRR       EEEEEEE     EEEEEEE     HHHH      HHHHH     PPPPPP      OOOOOOOOOOOOOO     TTTTTTTTTTTTTT   SSSSSSSSSSSSSSSSSS     IIII      IIIII    RRRRRRR     RRRRRRR     HHHHHHHHHHHHHHHHHHH    CCCC      CCCCC");
+		System.out.println("  RRRRRRRRR        RRRRRR     EEEEEEE     EEEEEEE     HHHH       HHHHH    PPPPPP     OOOOOO      OOOOO        TTTT       SSSSS          SSSSS    IIII       IIIII   RRRRRRR     RRRRRRR     HHHHHHH                CCCC       CCCCC");
+		System.out.println("  RRRRRRR           RRRRR     EEEEEEEEEEEEEEEEEEE     HHHH      HHHHH     PPPPPP    OOOOOO                    TTTT       SSSS            SSSS    IIII      IIIII    RRRRRRRRRRRRRRRRRRR     HHHHHHH                CCCC      CCCCC");
+		System.out.println("  RRRRRR                      EEEEEEEEEEEEEEEEEEE     HHHHHHHHHHHHH       PPPPPP    OOOOOOOOOOO               TTTT       SSSS            SSSS    IIIIIIIIIIIIII     RRRRRRRRRRRRRRRRRRR     HHHHHHHHHHHHHHHHHHH    CCCCCCCCCCCCC");
+		System.out.println("  RRRRRRR           RRRRR     EEEEEEEEEEEEEEEEEEE     HHHH HHHHHHH        PPPPPP          OOOOOOOOOO          TTTT       SSSS            SSSS    IIII               RRRRRRRRRRRRRRRRRRR     HHHHHHH                CCCC CCCCCCC");
+		System.out.println("   RRRRRRR        RRRRRR      EEEEEEE     EEEEEEE     HHHH   HHHHHHH      PPPPPP    OOOOO       OOOOOO        TTTT       SSSSS          SSSSS    IIII               RRRRRRR     RRRRRRR     HHHHHHH                CCCC   CCCCCCC");
+		System.out.println("    RRRRRRRRRRRRRRRRRRR       EEEEEEE     EEEEEEE     HHHH     HHHHHH     PPPPPP     OOOOO     OOOOOOO        TTTT        SSSSSSSSSSSSSSSSSS     IIII               RRRRRRR     RRRRRRR     HHHHHHHHHHHHHHHHHHH    CCCC     CCCCCC");
+		System.out.println("      RRRRRRRRRRRRRR          EEEEEEE     EEEEEEE     HHHH      HHHHHH    PPPPPP       OOOOOOOOOOOOO          TTTT          SSSSSSSSSSSSSS       IIII               RRRRRRR     RRRRRRR     HHHHHHHHHHHHHHHHHHH    CCCC      CCCCCC");
+	}
+ 
+ }
